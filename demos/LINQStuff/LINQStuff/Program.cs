9b118fc0955d68da915621c22a55ac421da2d67e
Simon Walker <simonw@nait.ca>
2026-02-05 09:28:40 -0700

    LINQ comparison demo added

---
+++ demos/LINQStuff/LINQStuff/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+
+namespace LINQStuff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stuff = { 1, 2, 3, 8, 12, -5 };
+
+            {
+                var res = stuff.Where((q) => q < 10).Select((q) => new { d = q * 2, h = q / 2 });
+                Console.WriteLine(string.Join(", ", res));
+            }
+
+            {
+                var res = from n in stuff where n < 10 select new { d = n * 2, h = n / 2 };
+                Console.WriteLine(string.Join(", ", res));
+            }
+        }
+    }
+}
